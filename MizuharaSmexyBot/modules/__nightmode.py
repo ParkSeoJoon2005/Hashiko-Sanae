eec551acf211cb421ce9b6040aa199493d0e9f52
Pain <86664546+AnuragSharma080@users.noreply.github.com>
2021-08-23 12:05:31 +0530

    Update __nightmode.py

--- MizuharaSmexyBot/modules/__nightmode.py
+++ MizuharaSmexyBot/modules/__nightmode.py
@@ -155,7 +155,7 @@ scheduler.start()
 
 
 __help__ = f"""
- ❍ /nightmode on/off
+ ➥ /nightmode on/off
  
 **Note:** Night Mode chats get Automatically closed at 12pm(IST)
 and Automatically openned at 6am(IST) To Prevent Night Spams.
