b419b296d44727c58e699244bd7e54d2f9b337f9
Pain <86664546+AnuragSharma080@users.noreply.github.com>
2021-09-05 17:21:44 +0530

    Update fakeit.py

--- MizuharaSmexyBot/modules/fakeit.py
+++ MizuharaSmexyBot/modules/fakeit.py
@@ -1,17 +1,3 @@
-#    Copyright (C) @chsaiujwal 2020-2021
-#    Edited by TeamDaisyX
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
 import os
 
 import requests
@@ -19,8 +5,8 @@ from faker import Faker
 from faker.providers import internet
 from telethon import events
 
-from DaisyX.function.telethonbasics import is_admin
-from DaisyX.services.telethon import tbot
+from MizuharaSmexyBot.function.telethonbasics import is_admin
+from MizuharaSmexyBot.services.telethon import tbot
 
 
 @tbot.on(events.NewMessage(pattern="/fakegen$"))
