a69808ceb95f53ae9078442da67e64052ae1a687
Pain <86664546+AnuragSharma080@users.noreply.github.com>
2021-09-07 11:59:12 +0530

    Update fakeit.py

--- MizuharaSmexyBot/modules/fakeit.py
+++ MizuharaSmexyBot/modules/fakeit.py
@@ -45,7 +45,7 @@ async def _(event):
             with open("FRIDAYOT.jpg", "wb") as f:
                 f.write(response.content)
 
-        captin = f"Fake Image powered by @DaisySupport_Official."
+        captin = f"Fake Image powered by @Project_Tsukiyomi."
         fole = "FRIDAYOT.jpg"
         await tbot.send_file(event.chat_id, fole, caption=captin)
         await event.delete()
