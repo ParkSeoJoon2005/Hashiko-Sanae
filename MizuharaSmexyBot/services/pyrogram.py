df075e9edb2e48096ad0f80d2129e5b5f7850f8f
Pain <86664546+AnuragSharma080@users.noreply.github.com>
2021-09-09 01:36:13 +0530

    Update pyrogram.py

--- MizuharaSmexyBot/services/pyrogram.py
+++ MizuharaSmexyBot/services/pyrogram.py
@@ -1,24 +1,9 @@
-# This file is part of DaisyXBot (Telegram Bot)
-
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-
-
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 import logging
 
 from pyrogram import Client
 
 # from pyromod import listen
-from DaisyX.config import get_int_key, get_str_key
+from MizuharaSmexyBot.config import get_int_key, get_str_key
 
 TOKEN = get_str_key("TOKEN", required=True)
 APP_ID = get_int_key("APP_ID", required=True)
