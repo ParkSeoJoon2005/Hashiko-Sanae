9c999f229ce2c4827eb128e12ea994fdb1fad93c
Pain <86664546+AnuragSharma080@users.noreply.github.com>
2021-09-09 11:27:36 +0530

    Update nsfw_watch_sql.py

--- MizuharaSmexyBot/modules/sql_extended/nsfw_watch_sql.py
+++ MizuharaSmexyBot/modules/sql_extended/nsfw_watch_sql.py
@@ -1,23 +1,10 @@
-#    Copyright (C) Midhun KM 2020-2021
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
 from sqlalchemy import Column, String
 
-from DaisyX.services.sql import BASE, SESSION
+from MizuharaSmexyBot.services.sql import BASE, SESSION
 
 
 class Nsfwatch(BASE):
-    __tablename__ = "nsfwatch"
+    __modname__ = "nsfwatch"
     chat_id = Column(String(14), primary_key=True)
 
     def __init__(self, chat_id):
