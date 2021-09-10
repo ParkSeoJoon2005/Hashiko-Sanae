f9b0b9390d4e9e0bb051fc00e3a4381c36d25808
Pain <86664546+AnuragSharma080@users.noreply.github.com>
2021-09-10 20:35:31 +0530

    Update animequotes.py

--- MizuharaSmexyBot/modules/animequotes.py
+++ MizuharaSmexyBot/modules/animequotes.py
@@ -1,42 +1,24 @@
 import html
-
 import random
-
 import time
-
 from telegram import ParseMode, Update, ChatPermissions
-
 from telegram.ext import CallbackContext, run_async
-
 from telegram.error import BadRequest
-
-import SaitamaRobot.modules.animequotes_strings as animequotes_strings
-
-from SaitamaRobot import dispatcher
-
-from SaitamaRobot.modules.disable import DisableAbleCommandHandler
-
-from SaitamaRobot.modules.helper_funcs.chat_status import (is_user_admin)
-
-from SaitamaRobot.modules.helper_funcs.extraction import extract_user
-
+import MizuharaSmexyBot.modules.animequotes_strings as animequotes_strings
+from MizuharaSmexyBot import dispatcher
+from MizuharSmexyBot.modules.disable import DisableAbleCommandHandler
+from MizuharaSmexyBot.modules.helper_funcs.chat_status import (is_user_admin)
+from MizuharaSmexyBot.modules.helper_funcs.extraction import extract_user
 @run_async
-
 def animequotes(update: Update, context: CallbackContext):
-
     message = update.effective_message
-
     name = message.reply_to_message.from_user.first_name if message.reply_to_message else message.from_user.first_name
-
     reply_photo = message.reply_to_message.reply_photo if message.reply_to_message else message.reply_photo
-
     reply_photo(
-
         random.choice(animequotes_strings.QUOTES_IMG))
-
 __help__ = """
 
- • `/animequotes`*:* gives random anime quotes
+ ➥ /animequotes : gives random anime quotes
 
  
 
@@ -46,7 +28,7 @@ ANIMEQUOTES_HANDLER = DisableAbleCommandHandler("animequotes", animequotes)
 
 dispatcher.add_handler(ANIMEQUOTES_HANDLER)
 
-__mod_name__ = "AnimeQuotes"
+__mod_name__ = "Anime-Quotes"
 
 __command_list__ = [
 
