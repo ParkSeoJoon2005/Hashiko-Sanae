96f53471f1f406a514513fa9eed0876ec7b7297c
Pain <86664546+AnuragSharma080@users.noreply.github.com>
2021-09-10 18:47:32 +0530

    Update anime.py

--- MizuharaSmexyBot/modules/anime.py
+++ MizuharaSmexyBot/modules/anime.py
@@ -720,20 +720,20 @@ __help__ = """
 ✨ *Fetch information about anime, manga or characters from* Anilist.co
 
 ⚙️ *Available commands:*
- • /detect : reply to an anime GIF / Video / Image for its sauce.
- • /quote: random quotes from anime characters.                              
- • /anime <anime> : returns information about the anime.
- • /airing <anime> : returns anime airing info.
- • /manga <manga> : returns information about the manga.
- • /character <character> : returns information about the character.
- • /user <user> : returns information about a MyAnimeList user.
- • /schedule : returns a list of new anime in the upcoming seasons.
- • /watchlist: to get your saved watchlist.
- • /mangalist: to get your saved manga read list.
- • /mywaifus | characterlist | fcl: to get your favorite characters / waifus list.
- • /removewatchlist | rwl <anime>: to remove a anime from your list.
- • /rfcharacter | rfcl <character>: to remove a character from your list.  
- • /rmanga | rml <manga>: to remove a manga from your list.                                
+ ➥ /detect : reply to an anime GIF / Video / Image for its sauce.
+ ➥ /quote: random quotes from anime characters.                              
+ ➥/anime <anime> : returns information about the anime.
+ ➥ /airing <anime> : returns anime airing info.
+ ➥ /manga <manga> : returns information about the manga.
+ ➥ /character <character> : returns information about the character.
+ ➥ /user <user> : returns information about a MyAnimeList user.
+ ➥ /schedule : returns a list of new anime in the upcoming seasons.
+ ➥ /watchlist: to get your saved watchlist.
+ ➥ /mangalist: to get your saved manga read list.
+ ➥/mywaifus | characterlist | fcl: to get your favorite characters / waifus list.
+ ➥ /removewatchlist | rwl <anime>: to remove a anime from your list.
+ ➥ /rfcharacter | rfcl <character>: to remove a character from your list.  
+ ➥ /rmanga | rml <manga>: to remove a manga from your list.                                
  """
 
 ANIME_HANDLER = DisableAbleCommandHandler("anime", anime)
