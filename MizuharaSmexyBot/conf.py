4b3e8f5086f9cd7000040c9bd760b047621d1ad1
Pain <86664546+AnuragSharma080@users.noreply.github.com>
2021-09-14 17:55:01 +0530

    Update conf.py

--- MizuharaSmexyBot/conf.py
+++ MizuharaSmexyBot/conf.py
@@ -31,7 +31,7 @@ class Config(object):
     WEBHOOK = False
     INFOPIC = True
     URL = None
-    SPAMWATCH_API = ""  # go to support.spamwat.ch to get key
+    SPAMWATCH_API = "CtEPYkywhKvUKM2i_oHXDPSUdzH7_~2SlDJyzMegz757RlHmDiALt2b8kKptTf_V"  # go to support.spamwat.ch to get key
     SPAMWATCH_SUPPORT_CHAT = "@SpamWatchSupport"
 
     #OPTIONAL
