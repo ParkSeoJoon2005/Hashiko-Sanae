409fd1100dacb55d4a16647dac6791442f9303ba
Pain <86664546+AnuragSharma080@users.noreply.github.com>
2021-09-17 12:46:10 +0530

    Update http.py

--- MizuharaSmexyBot/utils/http.py
+++ MizuharaSmexyBot/utils/http.py
@@ -1,29 +1,6 @@
-"""
-MIT License
-
-Copyright (c) 2021 TheHamkerCat
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-"""
 from asyncio import gather
 
-from wbb import aiohttpsession as session
+from MizuharaSmexyBot import dispatcher
 
 
 async def get(url: str, *args, **kwargs):
@@ -36,7 +13,7 @@ async def get(url: str, *args, **kwargs):
 
 
 async def head(url: str, *args, **kwargs):
-    async with session.head(url, *args, **kwargs) as resp:
+    async with dispatcher.head(url, *args, **kwargs) as resp:
         try:
             data = await resp.json()
         except Exception:
@@ -45,7 +22,7 @@ async def head(url: str, *args, **kwargs):
 
 
 async def post(url: str, *args, **kwargs):
-    async with session.post(url, *args, **kwargs) as resp:
+    async with dispatcher.post(url, *args, **kwargs) as resp:
         try:
             data = await resp.json()
         except Exception:
@@ -66,7 +43,7 @@ async def multipost(url: str, times: int, *args, **kwargs):
 
 
 async def resp_get(url: str, *args, **kwargs):
-    return await session.get(url, *args, **kwargs)
+    return await dispatcher.get(url, *args, **kwargs)
 
 
 async def resp_post(url: str, *args, **kwargs):
