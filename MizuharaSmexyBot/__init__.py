9a30b5c7efc94677d33306280250014ad2eda5aa
Pain <86664546+AnuragSharma080@users.noreply.github.com>
2021-09-17 12:53:50 +0530

    Update __init__.py

--- MizuharaSmexyBot/__init__.py
+++ MizuharaSmexyBot/__init__.py
@@ -21,7 +21,7 @@ logging.basicConfig(
 LOGGER = logging.getLogger(__name__)
 
 # if version < 3.6, stop bot.
-if sys.version_info[0] < 3 or sys.version_info[1] < 6:
+if sys.version_info[1] < 3 or sys.version_info[1] < 6:
     LOGGER.error(
         "You MUST have a python version of at least 3.6! Multiple features depend on this. Bot quitting."
     )
