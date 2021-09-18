a0f3829211ac29bebb2464d37551cecdff7e5955
Pain <86664546+AnuragSharma080@users.noreply.github.com>
2021-09-18 17:28:53 +0530

    Update lyrics.py

--- MizuharaSmexyBot/modules/lyrics.py
+++ MizuharaSmexyBot/modules/lyrics.py
@@ -3,8 +3,8 @@ from tswift import Song
 from telegram import Bot, Update, Message, Chat
 from telegram.ext import run_async
 
-from JisooX import dispatcher
-from JisooX.modules.disable import DisableAbleCommandHandler
+from MizuharaSmexyBot import dispatcher
+from MizuharaSmexyBot.modules.disable import DisableAbleCommandHandler
 
 
 @run_async
