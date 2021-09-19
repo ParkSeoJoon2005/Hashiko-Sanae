a11960c76b8663280ecef9a56ffac43c843587f7
Pain <86664546+AnuragSharma080@users.noreply.github.com>
2021-09-20 00:28:09 +0530

    Update chat_status.py

--- MizuharaSmexyBot/modules/helper_funcs/chat_status.py
+++ MizuharaSmexyBot/modules/helper_funcs/chat_status.py
@@ -1,10 +1,10 @@
 from functools import wraps
 from telegram import Bot, Chat, ChatMember, Update, User, ParseMode
-from MizuharaSmexyBot import dispatcher, DEL_CMDS, WHITELIST_USERS, SUPPORT_USERS, SUDO_USERS, DEV_USERS
+from MizuharaSmexyBot import dispatcher, DEL_CMDS, SUPPORT_USERS, SUDO_USERS, DEV_USERS
 
 
 def is_whitelist_plus(chat: Chat, user_id: int, member: ChatMember = None) -> bool:
-    return user_id in WHITELIST_USERS or user_id in SUPPORT_USERS or user_id in SUDO_USERS or user_id in DEV_USERS
+    return user_id in SUPPORT_USERS or user_id in SUDO_USERS or user_id in DEV_USERS
 
 
 def is_support_plus(chat: Chat, user_id: int, member: ChatMember = None) -> bool:
