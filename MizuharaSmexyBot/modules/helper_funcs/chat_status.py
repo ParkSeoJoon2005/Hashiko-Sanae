2d2b7be01c7b7b6caf672024681d8d24e6bf824d
Pain <86664546+AnuragSharma080@users.noreply.github.com>
2021-09-20 17:48:56 +0530

    Update chat_status.py

--- MizuharaSmexyBot/modules/helper_funcs/chat_status.py
+++ MizuharaSmexyBot/modules/helper_funcs/chat_status.py
@@ -1,7 +1,7 @@
 from functools import wraps
 from cachetools import TTLCache
 from threading import RLock
-from MizuaharaSmexyBot import (DEL_CMDS, DEV_USERS, DRAGONS, SUPPORT_CHAT, DEMONS,
+from MizuharaSmexyBot import (DEL_CMDS, DEV_USERS, DRAGONS, SUPPORT_CHAT, DEMONS,
                           TIGERS, WOLVES, dispatcher)
 
 from telegram import Chat, ChatMember, ParseMode, Update
