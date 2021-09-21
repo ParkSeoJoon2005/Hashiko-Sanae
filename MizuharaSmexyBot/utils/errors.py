9550f44ef11af0f7fd2b329bc1a6a0f7bb353782
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-09-21 20:35:26 +0700

    Change the code formation

--- MizuharaSmexyBot/utils/errors.py
+++ MizuharaSmexyBot/utils/errors.py
@@ -1,12 +1,9 @@
 import sys
 import traceback
 from functools import wraps
-
+from MizuharaSmexyBot import pbot, SUPPORT_CHAT
 from pyrogram.errors.exceptions.forbidden_403 import ChatWriteForbidden
 
-from MizuharaSmexyBot import pbot as app
-
-LOG_GROUP_ID = int(-534575324)
 
 def split_limits(text):
     if len(text) < 2048:
@@ -21,8 +18,8 @@ def split_limits(text):
         else:
             result.append(small_msg)
             small_msg = line
-    else:
-        result.append(small_msg)
+        
+    result.append(small_msg)
 
     return result
 
@@ -33,7 +30,7 @@ def capture_err(func):
         try:
             return await func(client, message, *args, **kwargs)
         except ChatWriteForbidden:
-            await app.leave_chat(message.chat.id)
+            await pbot.leave_chat(message.chat.id)
             return
         except Exception as err:
             exc_type, exc_obj, exc_tb = sys.exc_info()
@@ -49,8 +46,8 @@ def capture_err(func):
                 ),
             )
             for x in error_feedback:
-                await app.send_message(
-                    LOG_GROUP_ID,
+                await pbot.send_message(
+                    SUPPORT_CHAT,
                     x
                 )
             raise err
