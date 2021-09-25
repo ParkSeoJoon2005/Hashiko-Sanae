c29f4a4fd3e5acd7f113a1f49b62cdeb67098ef1
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-09-25 15:30:21 +0700

    Update and rename __tools.py to tools.py

--- MizuharaSmexyBot/modules/tools.py
+++ MizuharaSmexyBot/modules/tools.py
@@ -4,16 +4,12 @@ __help__ = """
 *Fake Info*
  ➥ /fakegen*:* Generates Fake Information
  ➥ /picgen*:* Generate a fake pic 
-
 *Date-time-Weather*
  ➥ /time <country code>*:* Gives information about a timezone.
  ➥ /weather <city>*:* Get weather info in a particular place.
  ➥ /wttr <city>*:* Advanced weather module, usage same as /weather
  ➥ /wttr moon*:* Get the current status of moon
  
-*Quotly*
-- /➥ : reply any message with this command to create a sticker.
-
 *Converts*
  ➥ /encrypt*:* Encrypts The Given Text
  ➥ /decrypt*:* Decrypts Previously Ecrypted Text
